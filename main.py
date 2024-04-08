76336c3a1a1f5ef6f1a194400b279d49334fa244
bobbyhadz <opmftw@gmail.com>
2024-04-08 14:29:47 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Count the number of Keys in a Dictionary in Python
+
+my_dict = {
+    'name': 'Bobby Hadz',
+    'age': 30,
+    'tasks': ['dev', 'test']
+}
+
+result_1 = len(my_dict)
+print(result_1)  # 👉️ 3
+
+result_2 = len(my_dict.keys())
+print(result_2)  # 👉️ 3
